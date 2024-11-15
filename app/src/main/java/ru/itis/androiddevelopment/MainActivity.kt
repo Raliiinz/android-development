0f5192f3becdbcf788d1e37b76905bf923f730b6
Raliiinz <garifullinaralina@gmail.com>
2024-11-15 18:33:36 +0300

    Initial Commit

--- app/src/main/java/ru/itis/androiddevelopment/MainActivity.kt
+++ app/src/main/java/ru/itis/androiddevelopment/MainActivity.kt
@@ -1,7 +1,26 @@
 package ru.itis.androiddevelopment
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.itis.androiddevelopment.databinding.ActivityMainBinding
 
-class MainActivity {
 
+class MainActivity : AppCompatActivity(){
+    val mainContainerId = R.id.main_fragment_container
+
+    private var viewBinding: ActivityMainBinding? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding?.root)
+
+
+    }
+
+    override fun onDestroy() {
+        viewBinding = null
+        super.onDestroy()
+    }
 }
 
