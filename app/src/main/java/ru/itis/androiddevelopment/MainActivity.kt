9d14c725ce80cfdc565c367a7a8196457b9e7060
Raliiinz <garifullinaralina@gmail.com>
2024-11-15 18:18:36 +0300

    Initial Commit

--- app/src/main/java/ru/itis/androiddevelopment/MainActivity.kt
+++ app/src/main/java/ru/itis/androiddevelopment/MainActivity.kt
@@ -1,34 +1,7 @@
 package ru.itis.androiddevelopment
 
-import android.os.Bundle
-import ru.itis.androiddevelopment.base.BaseActivity
-import ru.itis.androiddevelopment.base.NavigationAction
-import ru.itis.androiddevelopment.databinding.ActivityMainBinding
-import ru.itis.androiddevelopment.fragment.FirstFragment
 
-class MainActivity : BaseActivity() {
-    override val mainContainerId = R.id.main_fragment_container
+class MainActivity {
 
-    private var viewBinding: ActivityMainBinding? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(viewBinding?.root)
-
-        if (savedInstanceState == null) {
-            navigate(
-                destination = FirstFragment(),
-                destinationTag = FirstFragment.FIRST_TAG,
-                action = NavigationAction.REPLACE,
-                isAddToBackStack = false
-            )
-        }
-    }
-
-    override fun onDestroy() {
-        viewBinding = null
-        super.onDestroy()
-    }
 }
 
--- app/src/main/java/ru/itis/androiddevelopment/base/NavigationAction.kt
+++
@@ -1,5 +0,0 @@
-package ru.itis.androiddevelopment.base
-
-enum class NavigationAction {
-    ADD, REPLACE, REMOVE, HIDE
-}
